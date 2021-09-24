ceeac2c6985faef02b5f2097032e0f7293749080
Richard Trotta <trotta@cua.edu>
2021-09-23 20:17:45 -0400

    Trying to figure out why batch script isn't working

--- batch_scripts/batch_KaonLT.sh
+++ batch_scripts/batch_KaonLT.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2021-09-23 20:16:57 trottar"
+# Time-stamp: "2021-09-23 20:17:40 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -44,8 +44,8 @@ ANASCRIPT='"${UTILPATH}/Analysis_Scripts/run_KaonLT.sh" ${RUNTYPE}'
 ##Output history file##
 historyfile=hist.$( date "+%Y-%m-%d_%H-%M-%S" ).log
 ##Input run numbers##
-#inputFile='${UTILPATH}/InputRunLists/Kaon_Data/${RUNTYPE}_ALL'
-inputFile='${UTILPATH}/InputRunLists/Kaon_Data/${RUNTYPE}_Test'
+#inputFile="${UTILPATH}/InputRunLists/Kaon_Data/${RUNTYPE}_ALL"
+inputFile="${UTILPATH}/InputRunLists/Kaon_Data/${RUNTYPE}_Test"
 ## Tape stub
 MSSstub='/mss/hallc/spring17/raw/coin_all_%05d.dat'
 auger="augerID.tmp"
@@ -109,7 +109,7 @@ while true; do
 		    echo "###############################################################################################################"
 		    echo " "
 		fi
-	    done < "${inputFile}"
+	    done < "$inputFile"
 	    )
 	    break;;
         [Nn]* ) 
